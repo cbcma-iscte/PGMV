724e081112c9ae2273c10a92547426b42d23dd08
Bruno Carvalho <brunomscbruno@gmail.com>
2024-04-19 16:06:18 +0100

    New GameManager
    Attention Board in game Manager is just creationg of an object for debug proposes. When Board properly created, change accordingly

--- PGMV_Group2/Assets/Scripts/Structures/Board.cs
+++ PGMV_Group2/Assets/Scripts/Structures/Board.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Board : MonoBehaviour
+public class Board
 {
     
     public List<Tile> Tiles { get; set; }
     public int Width { get; set; }
     public int Height { get; set; }
 
+    public void InitializeBoard(int width, int height, List<Tile> tiles){
+        Width = width;
+        Height = height;
+        Tiles = tiles;
+    }
 }
