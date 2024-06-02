7cfd6ddc575f73a5ecd172b2b06fcf2796c6796a
Bruno Carvalho <61797027+BrunixD@users.noreply.github.com>
2024-06-02 18:16:17 +0100

    Update
    Animator control done
    flags created
    camera focused
    missing script , TODO

---
+++ PGMV_Group2/Assets/Scripts/Animation.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+
+    [SerializeField] GameObject attacker;
+    [SerializeField] GameObject defender;
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
